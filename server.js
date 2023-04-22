c82afa0f1957a78ee924e0f2cc90f104defb87dd
ReynagaE <123265741+ReynagaE@users.noreply.github.com>
2023-04-21 19:18:44 -0600

    fourth commit

--- server.js
+++ server.js
@@ -41,6 +41,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) }) 
 
-app.listen(process.env.port, ()=> {
-    console.log(`app is running on port ${process.env.port}`);
+app.listen(process.env.PORT, ()=> {
+    console.log(`app is running on port ${process.env.PORT}`);
 })
